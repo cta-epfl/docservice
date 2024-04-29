eeadd4a54282c845222cd872b7120d80710ddecf
Bastien <bastien.wermeille@gmail.com>
2024-04-29 07:59:14 +0200

    Fix lint issues

--- tests/test_updown.py
+++ tests/test_updown.py
@@ -3,8 +3,9 @@ import pytest
 from flask import url_for
 import requests
 
+
 @pytest.mark.timeout(30)
 def test_health(app: Any, client: Any):
     print(url_for('default'))
     with pytest.raises(requests.exceptions.ConnectionError):
-        r = client.get(url_for('default'))
+        client.get(url_for('default'))
